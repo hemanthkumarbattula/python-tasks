63977ec977e9d132f23c447c8ebc2fb4ed332d10
ebathem <hemanthkumarbattula@gmail.com>
2022-03-28 11:23:03 +0200

    adding test

---
+++ Big-o/analyze.py
@@ -0,0 +1,39 @@
+#Dynamic array
+
+import ctypes
+
+
+class DynamicArray():
+
+    def __init__(self):
+        self.n = 0
+        self.capacity = 1
+        self.A = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, k):
+        if not 0 <= k <= self.n:
+            return IndexError('k is out of index')
+        return slef.A[k]
+
+    def append(self, ele):
+        if self.n == self.capacity:
+            self.n = self._resize(2 * self.capacity)
+        self.A[self.n] = ele
+        self.n += 1
+
+    def _resize(self, cap):
+        B = self.make_array(cap)
+        for k in range(cap):
+            B[k] = self.A[k]
+        self.A = B
+        self.capacity = cap
+
+    def make_array(self, c):
+        return(c * ctypes.py_object)()
+
+arr = DynamicArray()
+arr.append(1)
+print(len(arr))
